c9738f9b7cd0e71a7962b0c5933bff55e9b4ca5f
Lanius <98239679+laniusone@users.noreply.github.com>
2022-01-23 00:05:45 +0100

    Update download-chrome-extensions-button.js
    Added an icon

--- download-chrome-extensions-button.js
+++ download-chrome-extensions-button.js
@@ -3,9 +3,10 @@
 // @namespace   laniusone
 // @match       https://chrome.google.com/webstore/detail/*/*
 // @grant       none
+// @icon        https://www.chromium.org/_/rsrc/1354323194313/favicon.ico
 // @version     1.0
 // @author      laniusone
-// @description Script meant for Ungoogled Chromium, but may be used with any browser to directly download Chrome Extensions from Chrome Web Store. It adds floating button to download extensions from Chrome Web Store in bottom right corner.
+// @description Script meant for Ungoogled Chromium, but may be used with any browser to directly download Chrome Extensions from Chrome Web Store.
 // ==/UserScript==
 let bookmarklet = function () {location.href='https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion='+(navigator.appVersion.match(/Chrome\/(\S+)/)[1])+'&x=id%'+'3D'+(document.querySelector('a[href^="https://chrome.google.com/webstore/report/"]').pathname.match(/[^\/]+\/*$/)[0])+'%'+'26installsource%'+'3Dondemand%'+'26uc';};
 button = document.createElement('button');
